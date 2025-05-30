320e7854b00b7b98b1b6046d498dd1f48e5d8132
vishnu <cmdvishnucp2018@gmail.com>
2025-05-30 19:50:29 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,26 @@
+function sendmail() {
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message").value;
+
+        if (name && email && message) {
+          alert("Message sent successfully!");
+          document.querySelector("form").reset();
+        } else {
+          alert("Please fill in all fields.");
+        }
+      }
+      document.querySelectorAll("#navbar a").forEach((link) => {
+        link.addEventListener("click", function (e) {
+          if (this.getAttribute("href").startsWith("#")) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href");
+            const targetSection = document.querySelector(targetId);
+            if (targetSection) {
+              targetSection.scrollIntoView({
+                behavior: "smooth",
+              });
+            }
+          }
+        });
+      });
