d363f6789210a93fc16dc5731da3749de0b2c5a0
vishnu <cmdvishnucp2018@gmail.com>
2025-05-31 13:06:21 +0530

    second commit

--- sample.js
+++ sample.js
@@ -1,26 +1,31 @@
 function sendmail() {
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const message = document.getElementById("message").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
 
-        if (name && email && message) {
-          alert("Message sent successfully!");
-          document.querySelector("form").reset();
-        } else {
-          alert("Please fill in all fields.");
-        }
-      }
-      document.querySelectorAll("#navbar a").forEach((link) => {
-        link.addEventListener("click", function (e) {
-          if (this.getAttribute("href").startsWith("#")) {
-            e.preventDefault();
-            const targetId = this.getAttribute("href");
-            const targetSection = document.querySelector(targetId);
-            if (targetSection) {
-              targetSection.scrollIntoView({
-                behavior: "smooth",
-              });
-            }
-          }
+  if (name && email && message) {
+    alert("Message sent successfully!");
+    document.querySelector("form").reset();
+  } else {
+    alert("Please fill in all fields.");
+  }
+}
+document.querySelectorAll("#navbar a").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    if (this.getAttribute("href").startsWith("#")) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href");
+      const targetSection = document.querySelector(targetId);
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: "smooth",
         });
-      });
+      }
+    }
+  });
+});
+
+function toggleMenu() {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("active");
+}
